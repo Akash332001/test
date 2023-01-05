b2411fca440fc0b4a5f3f7fa40aa9ae073cc4e3a
Akash <akash332001@gmail.com>
2023-01-06 04:56:53 +0530

    Change made

--- file1.py
+++ file1.py
@@ -1,5 +1,7 @@
 a=input("Enter Nationality")
 if(a=="English"):
-    print("Hello")
+    print("Mount")
+elif (a=="Spanish"):
+    print("Pedri")
 else:
-    print("Hola")    
+    print("Tchoumeni")    
